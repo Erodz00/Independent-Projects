4d44ff8736cdd082ed1e61482406f227a82b98d7
Enrique Rodriguez <soraestafar@gmail.com>
2018-11-10 23:08:56 -0500

    Update TextFlipper.java

--- TextFlipper.java
+++ TextFlipper.java
@@ -5,10 +5,12 @@ public class TextFlipper {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number of Strings you want to flip:");
 		int n = scan.nextInt();
 		scan.nextLine();
 		ArrayList<String> arr = new ArrayList<String>();
 		for (int i = 0; i < n; i++) {
+			System.out.println("Enter a String:");
 			String str = scan.nextLine();
 			StringBuilder sb = new StringBuilder(str);
 			for (int j = 1; j < str.length()+1; j++) {
